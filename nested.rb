2dce23ebb6c3c5f530454ba2a7c818555b53f9a0
Hailey Bongo <haileyebongo@gmail.com>
2020-02-03 17:07:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return programmer_hash[:dennis_ritchie][:languages]
+     language = programmer_hash[:dennis_ritchie][:languages]
+     return language[0]
 end
 
 def adding_matz
